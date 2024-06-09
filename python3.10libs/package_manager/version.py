9029ddd6f05e1cde52d6b05f337fb722da9689f1
Ivan Titov <risenforce@mail.ru>
2024-06-09 15:40:07 +0300

    Version module type annotated.

--- python3.10libs/package_manager/version.py
+++ python3.10libs/package_manager/version.py
@@ -1,16 +1,14 @@
-# coding: utf-8
-
-from __future__ import print_function
+from typing import Union
 
 
 class Token(object):
-    def __new__(cls, source):
+    def __new__(cls, source) -> 'Token':
         if isinstance(source, Token):
             return source
         else:
             return super(Token, cls).__new__(cls)
 
-    def __init__(self, source):
+    def __init__(self, source: str) -> None:
         if isinstance(source, Token):
             return
 
@@ -21,20 +19,20 @@ class Token(object):
             self.__value = str(source).strip().lower()
 
     @property
-    def raw(self):
+    def raw(self) -> str:
         return self.__raw
 
     @property
-    def value(self):
+    def value(self) -> str | int:
         return self.__value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return 'Token("{0}")'.format(self.raw)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.__raw
 
-    def __eq__(self, other):
+    def __eq__(self, other: Union['Token', int, str]) -> bool:
         if isinstance(other, Token) and isinstance(other.value, int):
             return self.value == other.value
         elif isinstance(other, int) and isinstance(self.value, int):
@@ -46,7 +44,7 @@ class Token(object):
                 return self.value == other.value
         return NotImplemented
 
-    def __ne__(self, other):
+    def __ne__(self, other: Union['Token', int, str]) -> bool:
         if isinstance(other, Token) and isinstance(other.value, int):
             return self.value != other.value
         elif isinstance(self.value, int) and isinstance(other, int):
@@ -58,7 +56,7 @@ class Token(object):
                 return self.value != other.value
         return NotImplemented
 
-    def __lt__(self, other):
+    def __lt__(self, other: Union['Token', int, str]) -> bool:
         if isinstance(other, Token) and isinstance(other.value, int):
             return self.value < other.value
         elif isinstance(self.value, int) and isinstance(other, int):
@@ -77,7 +75,7 @@ class Token(object):
                 return False
         return NotImplemented
 
-    def __gt__(self, other):
+    def __gt__(self, other: Union['Token', int, str]) -> bool:
         if isinstance(other, Token) and isinstance(other.value, int):
             return self.value > other.value
         elif isinstance(self.value, int) and isinstance(other, int):
@@ -97,7 +95,7 @@ class Token(object):
         return NotImplemented
 
 
-def parseVersion(version_string):
+def parseVersion(version_string: str) -> tuple[tuple[Token, ...], tuple[Token, ...], str]:
     """Returns ((<number_tokens>), (<qualifier_tokens>), <build_metadata>)"""
     # Find end of the redundant characters at the beginning
     start_index = -1
@@ -154,13 +152,13 @@ def parseVersion(version_string):
 
 
 class Version(object):
-    def __new__(cls, source):
+    def __new__(cls, source: Union['Version', str]) -> 'Version':
         if isinstance(source, Version):
             return source
         else:
             return super(Version, cls).__new__(cls)
 
-    def __init__(self, source):
+    def __init__(self, source: Union['Version', str]) -> None:
         if isinstance(source, Version):
             return
 
@@ -175,37 +173,37 @@ class Version(object):
             self.__num_tokens = self.__num_tokens[:-first_nonzero_index]
 
     @property
-    def raw(self):
+    def raw(self) -> str:
         return self.__raw
 
     @property
-    def num_tokens(self):
+    def num_tokens(self) -> tuple[Token, ...]:
         return self.__num_tokens
 
     @property
-    def qualifier_tokens(self):
+    def qualifier_tokens(self) -> tuple[Token, ...]:
         return self.__qualifier_tokens
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return 'Version("{0}")'.format(self.__raw)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.__raw
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if isinstance(other, (str, Version)):
             other = Version(other)
             return self.num_tokens == other.num_tokens and \
-                   self.qualifier_tokens == other.qualifier_tokens
+                self.qualifier_tokens == other.qualifier_tokens
         else:
             return NotImplemented
 
-    def __ne__(self, other):
+    def __ne__(self, other) -> bool:
         other = Version(other)
         return self.num_tokens != other.num_tokens or \
-               self.qualifier_tokens != other.qualifier_tokens
+            self.qualifier_tokens != other.qualifier_tokens
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         other = Version(other)
         self_len = len(self.__num_tokens)
         other_len = len(other.num_tokens)
@@ -242,7 +240,7 @@ class Version(object):
                 return True
         return False
 
-    def __gt__(self, other):
+    def __gt__(self, other) -> bool:
         other = Version(other)
         self_len = len(self.__num_tokens)
         other_len = len(other.num_tokens)
@@ -289,7 +287,7 @@ class Version(object):
 
 
 class VersionRange(object):
-    def __init__(self, low_version=None, high_version=None):
+    def __init__(self, low_version: Version | None = None, high_version: Version | None = None) -> None:
         if low_version is None:
             self.low_version = Version('0')
         else:
@@ -301,7 +299,7 @@ class VersionRange(object):
             self.high_version = high_version
 
     @classmethod
-    def fromPattern(cls, pattern):
+    def fromPattern(cls, pattern: str) -> 'VersionRange':
         pattern = pattern.strip()
         if pattern == '*':
             low_version = Version('0')
@@ -318,21 +316,21 @@ class VersionRange(object):
             raise ValueError('Invalid version range pattern')
         return cls(low_version, high_version)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return 'Version Range [{0};{1}]'.format(self.low_version, self.high_version)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return 'VersionRange({0}, {1})'.format(self.low_version.__repr__(),
                                                self.high_version.__repr__())
 
-    def __contains__(self, item):
+    def __contains__(self, item: Union[str, Version, 'VersionRange']) -> bool:
         if isinstance(item, (str, Version)):
             version = Version(item)
             return self.low_version <= version <= self.high_version
         elif isinstance(item, VersionRange):
             pass
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if isinstance(other, Version):
             return other in self
         elif isinstance(other, VersionRange):
@@ -342,7 +340,7 @@ class VersionRange(object):
 
 
 class VersionPattern(object):
-    def __init__(self, pattern):
+    def __init__(self, pattern: str) -> None:
         self.__raw = str(pattern)
 
         self.__include = []
@@ -355,20 +353,20 @@ class VersionPattern(object):
                 self.__include.append(VersionRange.fromPattern(token))
 
     @property
-    def raw(self):
+    def raw(self) -> str:
         return self.__raw
 
-    def __contains__(self, item):
+    def __contains__(self, item: Union[str, Version, VersionRange, 'VersionPattern']) -> bool:
         if isinstance(item, (str, Version)):
             version = Version(item)
             return version in self.__include and \
-                   version not in self.__exclude
+                version not in self.__exclude
         elif isinstance(item, VersionRange):
             pass
         elif isinstance(item, VersionPattern):
             pass
 
-    def __eq__(self, other):
+    def __eq__(self, other: Union[str, Version, VersionRange, 'VersionPattern']) -> bool:
         if isinstance(other, Version):
             return other in self
         elif isinstance(other, VersionPattern):
@@ -383,6 +381,7 @@ if False:
     assert Version('6') < Version('10')
     assert '10.2.305' in VersionRange.fromPattern('6-10.3')
     assert '18.5' in VersionPattern('18+')
+    assert '17' not in VersionPattern('18+')
     assert Version('0.3-beta') == Version('0.3.0-beta')
     assert Version('0.4-beta') > Version('0.4-alpha')
     assert Version('0.4-beta.alpha') < Version('0.4-alpha')
